435687f1d03d7262885a8594225822444057e2ba
TeamK <koceto1973@yahoo.ca>
2018-12-11 15:49:07 +0200

    app structure in

---
+++ myApp.js
@@ -0,0 +1,22 @@
+// global app controller
+var appController = ( function(budjet,UI){
+
+    // acknowlede new entry
+    document.querySelector('.add__btn').addEventListener('click', function(){
+        console.log("Ouch!");
+    })
+
+})(budjetController,UIController);
+
+
+
+// budjet controller
+var budjetController = ( function(){
+
+}
+)();
+
+// ui controller
+var UIController =( function(){
+
+})();
