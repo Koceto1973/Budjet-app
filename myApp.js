ea8b8846b0ebd869808ddc27b0de463546247844
TeamK <koceto1973@yahoo.ca>
2018-12-12 00:12:08 +0200

    input data reading

--- myApp.js
+++ myApp.js
@@ -1,22 +1,61 @@
-// global app controller
-var appController = ( function(budjet,UI){
-
-    // acknowlede new entry
-    document.querySelector('.add__btn').addEventListener('click', function(){
-        console.log("Ouch!");
-    })
-
-})(budjetController,UIController);
-
-
-
 // budjet controller
 var budjetController = ( function(){
 
-}
-)();
+})();
 
 // ui controller
 var UIController =( function(){
+    
+    // single link to the css selectors
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type : document.querySelector(DOMstrings.inputType).value,
+                description : document.querySelector(DOMstrings.inputDescription).value,
+                value : document.querySelector(DOMstrings.inputValue).value
+            };            
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
 
 })();
+
+// global app controller
+var appController = ( function(budjet,UI){
+
+    var DOM = UI.getDOMstrings();
+
+    function addItem() {
+        // accept input
+
+        // update budjet
+
+        // update ui
+
+        // recalculate budjet
+
+        // display budjet
+
+        console.log("Ouch!");
+    }
+    // by click event
+    document.querySelector(DOM.inputBtn).addEventListener('click', addItem);
+    // or by keypress enter
+    document.addEventListener('keypress',function(event){
+        if ( event.keyCode === 13 || event.which === 13 ) { // event.which for older browsers
+            addItem();
+        }
+    });
+
+})(budjetController,UIController);
+
